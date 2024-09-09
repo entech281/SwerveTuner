d5afb81f560cece77105b0acf0310d3128d8b5fe
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2024-09-09 18:22:15 -0400

    vendor deps\

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SwerveTuner";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "04e94e01284be9340363560686e2a6b84d2f5ee6";
+  public static final String GIT_DATE = "2024-09-09 18:17:49 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-09-09 18:21:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1725920514884L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
